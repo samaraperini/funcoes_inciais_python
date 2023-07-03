9a721a51ef20d320d3c79e40e902e008ff3e1d3f
Samara Perini <138031738+samaraperini@users.noreply.github.com>
2023-07-03 15:59:39 -0300

    Add files via upload

---
+++ tuplas.py
@@ -0,0 +1,17 @@
+# TUPLAS
+
+tupla_colunas = ('Otávio', 26, 'Rua João da Silva', '29/10/2000')
+print(tupla_colunas)
+
+lista = [1,2,3]
+
+lista.append(tupla_colunas)
+print(lista)
+print(lista[-1])
+
+
+# As tuplas são imutáveis, o que significa que não é possível modificar seus elementos após a criação.
+# As tuplas tendem a ter um desempenho ligeiramente melhor do que as listas, pois são estruturas de dados mais simples e não precisam lidar com operações de modificação. Isso tende a ser relevante em situações nas quais a performance é um fator crítico.
+# As tuplas são definidas usando parênteses, enquanto as listas são definidas com colchetes. Por exemplo, uma tupla seria declarada como (1, 2, 3), e uma lista seria declarada como [1, 2, 3].
+# As tuplas são frequentemente usadas quando se deseja armazenar um conjunto de elementos
+
